8ca9e8b37a10c471cda6826e14e99caab040fcef
Melissa Iman Noriega <121834468+Melissa1221@users.noreply.github.com>
2023-09-10 23:47:40 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+const accessKey = "qGYY9aHBXscVICF5Y72zJV7im0YSWNYM9PWcvyqZCRQ"
+const formEl = document.querySelector("form")
+const inputEl = document.getElementById("search-input")
+const searchResults = document.getElementById("search-results")
+const showMore = document.getElementById("show-more-button")
+
+
+let inputData = ""
+let page = 1;
+
+async function searchImages(){
+    inputData = inputEl.value
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    const results = data.results
+
+    if (page == 1){
+        searchResults.innerHTML = ""
+    }
+
+    results.map((result) => {
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("search-result");
+        
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+    
+        imageLink.appendChild(image);
+        
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+    });
+    
+    page ++
+    if (page > 1){
+        showMore.style.display = "block"
+    }
+
+
+}
+
+formEl.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    page = 1;
+    searchImages()
+})
+
+showMore.addEventListener("click", (event)=>{
+    event.preventDefault()
+    page++;
+    searchImages()
+})
